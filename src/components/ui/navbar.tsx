801d387136624d59592a6f57520202757b88634e
Apurbo <akdjoy29@gmail.com>
2026-06-18 15:04:40 +0600

    nav done

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -5,8 +5,9 @@ import { Show, SignInButton, SignOutButton, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React, { useState } from "react";
-import { BsInstagram, BsSearch } from "react-icons/bs";
+import { BsChevronDown, BsInstagram, BsSearch } from "react-icons/bs";
 import { FaBlog, FaFacebookF, FaTwitter } from "react-icons/fa";
+import { HiOutlineMenu, HiOutlineX } from "react-icons/hi";
 import { IoMdMoon, IoMdSunny } from "react-icons/io";
 
 interface NavItem {
@@ -143,6 +144,149 @@ const Navbar = () => {
           </div>
         </div>
       </div>
+
+      {/* main navigation */}
+      <nav className="py-3 border-b border-gray-200">
+        {/* larger device */}
+        <div className="blog-container ">
+          {/* mobile menu toggle (hamburger) */}
+          <div className="lg:hidden flex items-center justify-between w-full">
+            <h1 className="shrink-0">
+              <Link
+                href="/"
+                className="text-2xl flex items-center font-semibold text-gray-900 hover:text-primary transition-colors duration-300"
+              >
+                <FaBlog />
+                <span className="ml-1">Blogs</span>
+              </Link>
+            </h1>
+            <button
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+              className="p-2 rounded-md text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none  transition-colors duration-300 lg:hidden"
+            >
+              {isMobileMenuOpen ? (
+                <HiOutlineX size={24} />
+              ) : (
+                <HiOutlineMenu size={24} />
+              )}
+            </button>
+          </div>
+
+          <ul className="hidden lg:flex items-center justify-between space-x-6">
+            {mainNavItems.map((item) => {
+              const isActive = pathName === item.href;
+              return (
+                <li key={item.label} className="group relative">
+                  <Link
+                    href={item.href}
+                    className={`inline-flex items-center text-sm uppercase font-medium rounded-md text-gray-500 hover:text-primary ${isActive ? " text-primary" : ""}`}
+                  >
+                    {item.label}
+                    {item.subItems && (
+                      <BsChevronDown
+                        size={16}
+                        className="ml-1 group-hover:rotate-180 transition-transform"
+                      />
+                    )}
+                  </Link>
+                  {item.subItems && (
+                    <ul className="absolute left-0 top-full mt-2 hidden group-hover:block bg-white shadow-lg rounded-md overflow-hidden z-20 min-w-57.5 border border-gray-200 ">
+                      {item.subItems.map((subItem) => (
+                        <li className="mt-1" key={subItem.label}>
+                          <Link
+                            href={subItem.href}
+                            className="block px-4 py-2 text-sm uppercase text-gray-700 hover:bg-gray-100 transition-colors"
+                          >
+                            {subItem.label}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        {/* mobile menu  */}
+        {isMobileMenuOpen && (
+          <div className="lg:hidden fixed inset-0 bg-white z-50 overflow-y-auto">
+            <div className="flex justify-end p-4">
+              <button
+                className="p-2 rounded-md text-gray-500 hover:text-gray-700 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-primary transition-colors duration-300"
+                onClick={() => setIsMobileMenuOpen(false)}
+              >
+                <HiOutlineX size={24} />
+              </button>
+            </div>
+            <ul className="flex flex-col p-4  space-y-2">
+              {mainNavItems.map((item) => {
+                const isActive = pathName === item.href;
+                return (
+                  <li key={item.label} className="group relative">
+                    <Link
+                      href={item.href}
+                      className={`inline-flex items-center text-sm uppercase font-medium rounded-md text-gray-500 hover:text-primary ${isActive ? " text-primary" : ""}`}
+                    >
+                      {item.label}
+                      {item.subItems && (
+                        <BsChevronDown
+                          size={16}
+                          className="ml-1 group-hover:rotate-180 transition-transform"
+                        />
+                      )}
+                    </Link>
+                    {item.subItems && (
+                      <ul className="absolute left-0 top-full mt-2 hidden group-hover:block bg-white shadow-lg rounded-md overflow-hidden z-20 min-w-57.5 border border-gray-200 ">
+                        {item.subItems.map((subItem) => (
+                          <li className="mt-1" key={subItem.label}>
+                            <Link
+                              href={subItem.href}
+                              className="block px-4 py-2 text-sm uppercase text-gray-700 hover:bg-gray-100 transition-colors"
+                            >
+                              {subItem.label}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </li>
+                );
+              })}
+              <li>
+                <Show when="signed-out">
+                  <SignInButton mode="modal">
+                    <button className="px-5 py-1.5 border border-gray-300 bg-green-600 text-white rounded-md hover:bg-green-700 hover:text-white transition-colors focus:outline-none focus:ring-2 focus:ring-primary cursor-pointer">
+                      Sign In
+                    </button>
+                  </SignInButton>
+                </Show>
+
+                <Show when="signed-in">
+                  <div className="flex items-center gap-3">
+                    <UserButton />
+
+                    {/* <SignOutButton>
+                  <button className="px-5 py-1.5 border border-gray-300 bg-green-600 text-white rounded-md hover:bg-green-700 hover:text-white transition-colors focus:outline-none focus:ring-2 focus:ring-primary cursor-pointer">
+                    Sign Out
+                  </button>
+                </SignOutButton> */}
+                  </div>
+                </Show>
+                {/* <SignedOut>
+                        <SignInButton mode='modal'>
+                            <button className='px-5 py-2 border border-gray-300 bg-green-600 text-white rounded-md hover:bg-green-700 hover:text-white trasition-colors focus:outline-none focus:ring-2 focus:ring-primary cursor-pointer'>Sign In</button>
+                        </SignInButton>
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton/>
+                    </SignedIn> */}
+              </li>
+            </ul>
+          </div>
+        )}
+      </nav>
     </header>
   );
 };
